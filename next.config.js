8329ea3af73e40aec3e806af42f8d07870eee057
Murilo Silvani <murilo.silvani@gmail.com>
2022-05-26 12:28:49 -0300

    fix homepage

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  assetPrefix: '/waterlifepro_web/',
 }
 
 module.exports = nextConfig
